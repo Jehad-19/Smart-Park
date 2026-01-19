66364433ea93984233623f87a3f9bbe3bb7354b1
Jehad <jeha710@gmail.com>
2026-01-20 01:26:15 +0200

    asf

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -10,7 +10,7 @@ use Laravel\Sanctum\HasApiTokens;
 
 class Admin extends Authenticatable
 {
-    use HasFactory, Notifiable, HasApiTokens;
+    use HasFactory, Notifiable, HasApiTokens, SoftDeletes;
 
     protected $fillable = [
         'name',
